0064ba347b292d05869725a6b71b76ab5c06408d
Justin Dudley <justindudley99@gmail.com>
2021-09-11 00:13:04 -0400

    display list of files to user

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const serveIndex = require('serve-index');
 
 // middleware
 app.use((req, res, next) => {
@@ -7,12 +8,18 @@ app.use((req, res, next) => {
     next();
   });
 
-
 app.use('/request-type', (req, res, next) => {
     res.send('You put request-type in the url');
     console.log('Request type: ', req.method);
     next();
   });
+
+
+// display list of all files in /public folder
+app.use('/public', express.static('public'));
+app.use('/public', serveIndex('public'));
+
+
 
 // tell our server how to handle a GET request
 // We can use the req object to read data about what the client is requesting to do.
